3822c45b0e28deb08b93c22ee93c60f4a0ea4e3a
Petr Pavlik <petrpavlik@me.com>
2025-06-11 17:25:39 +0200

    Initial Commit

---
+++ Macontainer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Macontainer
+//
+//  Created by Petr Pavlik on 11.06.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Macontainer/MacontainerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacontainerApp.swift
+//  Macontainer
+//
+//  Created by Petr Pavlik on 11.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct MacontainerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
